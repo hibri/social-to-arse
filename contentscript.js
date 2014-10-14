9478d546574614cc0bfd16361c8586f0d12d515e
Hibri Marzook <hibri@hibri.net>
2014-10-14 19:16:08 +0200

    Change all mentions of ebola to zombie

--- contentscript.js
+++ contentscript.js
@@ -1,18 +1,18 @@
 var walk = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, {
     acceptNode: function(node) {
-      return node.nodeValue.match(/social/i) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
+      return node.nodeValue.match(/ebola/i) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP;
     }
   }, false);
 
 while(node = walk.nextNode()) {
-  node.nodeValue = node.nodeValue.replace(/social/ig, function($match) {
+  node.nodeValue = node.nodeValue.replace(/ebola/ig, function($match) {
       switch ($match) {
-        case 'social':
-          return 'arse';
-        case 'Social':
-          return 'Arse';
-        case 'SOCIAL':
-          return 'ARSE';
+        case 'ebola':
+          return 'zombie';
+        case 'Ebola':
+          return 'zombie';
+        case 'EBOLA':
+          return 'ZOMBIE';
       }
   });
 }
